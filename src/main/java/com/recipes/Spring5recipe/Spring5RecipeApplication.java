c1d6bf9a3a019ee78d4fae4b75c44acdf883e267
hiteshkataria <kataria.hitesh@yahoo.in>
2019-04-06 18:14:57 +0530

    initialized the recipe spring 5 framework and added .gitignore file

---
+++ src/main/java/com/recipes/Spring5recipe/Spring5RecipeApplication.java
@@ -0,0 +1,13 @@
+package com.recipes.Spring5recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5RecipeApplication.class, args);
+	}
+
+}
